37927ee75ce50d1b5b2fe5a18b3698ef53daec99
IuliiaKot <kotlenko.julia@gmail.com>
2016-08-29 17:33:29 -0400

    add nested json response

--- app/assets/javascripts/components/ListMovies.es6.jsx
+++ app/assets/javascripts/components/ListMovies.es6.jsx
@@ -12,6 +12,7 @@ class ListMovies extends React.Component {
       method: 'get',
       url: '/api/v1/films.json'
     }).done((response => {
+      // debugger
       this.setState({movies: response})
     })
 
@@ -19,13 +20,14 @@ class ListMovies extends React.Component {
   }
 
   render() {
+    // debugger
     return (
       <div className='container'>
         <div className='row text-center'>
           {this.state.movies.map((movie, idx) => {
             return (<div className='col-md-12'>
                       <header>{movie['title']}</header>
-                      <span></span>
+                      <span>{movie['category']['name']}</span>
                       <p>{movie['plot']}</p>
                       <img src={movie['poster']}></img>
                     </div>)
--- app/controllers/api/v1/films_controller.rb
+++ app/controllers/api/v1/films_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::FilmsController < Api::V1::BaseController
   def index
-    debugger
-    respond_with Film.all
+    # debugger
+    # respond_with (Category.first.as_json(include:[:films]))
+    respond_with (Film.all.to_json(include: :category))
   end
 end
